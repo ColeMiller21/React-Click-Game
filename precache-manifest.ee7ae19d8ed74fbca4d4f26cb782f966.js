70bf742b8ec9396d339b84481b3752eee72277ff
colemiller21 <cole.t.miller@gmail.com>
2020-02-06 21:32:04 -0600

    Updates

---
+++ precache-manifest.ee7ae19d8ed74fbca4d4f26cb782f966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80dc0731953cea7b1b7b998c7994df8",
+    "url": "/React-Click-Game/index.html"
+  },
+  {
+    "revision": "0f4f072671bc2da3f7cb",
+    "url": "/React-Click-Game/static/css/main.5f59ce49.chunk.css"
+  },
+  {
+    "revision": "c1c0dacf873f33021a6a",
+    "url": "/React-Click-Game/static/js/2.0ddcd4e5.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/React-Click-Game/static/js/2.0ddcd4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4f072671bc2da3f7cb",
+    "url": "/React-Click-Game/static/js/main.7a40980f.chunk.js"
+  },
+  {
+    "revision": "498d93d39245e335df64",
+    "url": "/React-Click-Game/static/js/runtime-main.b16a12c9.js"
+  },
+  {
+    "revision": "7b98344b4f207bb5153d12e9a611fff5",
+    "url": "/React-Click-Game/static/media/football.7b98344b.jpg"
+  }
+]);
